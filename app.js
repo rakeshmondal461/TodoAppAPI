36ca4b679a93f7bd75b421391bc4bc5248123632
rakeshmondal461 <mondalrakesh461@gmail.com>
2023-02-11 12:48:27 +0530

    database string updated

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const port = 3030;
 const taskRoute = require("./routes/taskRoute");
 const reminderRoute = require("./routes/reminderRoute");
 
-const MONGODB_URI = "mongodb://localhost:27017/todoApp";
+const MONGODB_URI =
+  "mongodb+srv://rakeshmondal:wzF1ATqV7y5r5Ijo@cluster0.rv3djdk.mongodb.net/todoAppAPI?retryWrites=true&w=majority";
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
